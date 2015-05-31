9cb183c29e03162e6b9984421f507e9208a16682
Sergey Chikuyonok <serge.che@gmail.com>
2015-06-01 00:09:11 +0300

    Create user session

--- index.js
+++ index.js
@@ -1,42 +1,36 @@
 'use strict';
 
 var koa = require('koa');
-var router = require('koa-router')();
+var bodyParser = require('koa-bodyparser');
 var mongoose = require('mongoose');
-var debug = require('debug')('rv:server');
-var sessionAuth = require('./lib/authorize');
+var router = require('./lib/router');
 
-var app = koa();
 mongoose.connect('mongodb://localhost:27017/rv');
 
-// Creates a new connection of user local host and publicly-available host name 
-// (aka reverse tunnel)
-router.post('/connect', function *(next) {
-	var auth = this.get('Authorization');
-	if (!auth) {
-		return this.throw(401, 'No authorization header');
+var app = koa()
+.use(bodyParser())
+.use(function *(next) {
+	try {
+		yield next;
+	} catch (err) {
+		// handle errors generated during routing
+		this.status = err.code || 500;
+		this.body = err.message;
+		this.app.emit('error', err, this);
 	}
 
-	debug('authorizing user');
-	var user = yield sessionAuth(auth);
-	debug('user received');
-	if (!user) {
-		return this.throw('User not created');
+	if (this.status >= 400 && typeof this.body === 'string') {
+		// respond with JSON message 
+		this.body = {
+			error: {
+				message: this.body,
+				code: this.status
+			}
+		};
 	}
-
-	// TODO create session
-	this.set('Location', 'http://w1.livestyle.io/sess-id');
-	this.status = 201;
-	this.body = {
-		email: user.email,
-		name: user.name
-	};
-});
-
-
-app
+})
 .use(router.routes())
-.use(router.allowedMethods())
-.listen(9004);
+.use(router.allowedMethods());
 
+app.listen(9004);
 console.log('Created server on 9004 port');
